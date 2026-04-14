f4271254535ed8e3260ca269e643ad5d43d6995e
simon.m.hofmann <simon.hofmann@cbs.mpg.de>
2026-04-14 11:30:04 +0200

    minor

--- code/tests/test_hyphi.py
+++ code/tests/test_hyphi.py
@@ -19,7 +19,7 @@ dist = norm(loc=0, scale=1)
 data = dist.rvs(1000)
 
 # Compute density estimates using Sheather Jones, compare to bw = 1
-# bw = 1 is optimal since underlying distribution has std. dev. = 1
+# bw = 1 is optimal since the underlying distribution has std. dev. = 1
 f_isj = fit_kde(data, bw="ISJ", method="tree")
 x_isj, y_isj = f_isj()
 
@@ -30,8 +30,8 @@ x_truth, y_truth = f_truth()
 # Replicate the Nature methods paper small world simulations
 pt_nat, Gt_nat = gen_nature_sw()
 
-FRCt_nat = get_frc_vec(Gt_nat)
+frc_t_nat = get_frc_vec(Gt_nat)
 
-Ht_nat = vec_entropy(FRCt_nat, get_entropy_kde_plugin)
+ht_nat = vec_entropy(frc_t_nat, get_entropy_kde_plugin)
 
 # o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o >><< o END
